14b6f0dc2c1a4970a6269e3b5f24a7048da59be5
Makai <danielmakai@gmail.com>
2024-10-24 02:52:34 +0300

    noSql

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""a Python function that inserts a new document
+in a collection based on kwargs:
+
+"""
+
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection"""
+    return mongo_collection.insert(kwargs)
